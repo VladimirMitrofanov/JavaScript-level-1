51e397d06c44c799f1aa85b49babfbe10aa79753
VladimirMitrofanov <vladimi-mitrofano@yandex.ru>
2021-01-27 09:24:47 +0300

    Практика Урок 1

---
+++ LESSON_1/main.js
@@ -0,0 +1,15 @@
+'use stict'
+
+var temperatureСelsius = prompt('Введите температуру по Цельсию');
+var temperatureFarengate = (9 / 5) * temperatureСelsius + 32;
+
+alert(temperatureFarengate);
+
+
+var admin;
+var name = 'Василий';
+admin = name;
+
+console.log(admin);
+
+// Чему будет равно JS-выражение 1000 + "108"? Ответ: 1000108
